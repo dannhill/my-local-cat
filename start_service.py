749b6d9e7d29fda35e3d767c2e88e6c2b6883d8a
Danilo Medas <medas.danilo@gmail.com>
2025-03-25 12:54:37 +0100

    added default cat configuration and python script to start service

---
+++ start_service.py
@@ -0,0 +1,11 @@
+#create folder ./cat/plugins and copy folder ./my_plugin to ./cat/plugins
+import os
+import shutil
+import subprocess
+
+#copy "default_cat" in the same directory and change the name of the copy to "cat"
+if not os.path.exists('./cat'):
+    shutil.copytree('./default_cat', './cat')
+
+#execute the command "docker compose up -d" in the current directory
+subprocess.run(['docker-compose', 'up', '-d'], check=True)
